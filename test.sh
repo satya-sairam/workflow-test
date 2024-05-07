532a7489ec804d63bfc058e352d18716cad27ea0
satya-sairam <kssairam@redcloudcomputing.com>
2024-05-07 17:31:32 +0530

    input ip test

--- test.sh
+++ test.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 echo "Hello"
+
+read -r username host1 host2 host3 < "$inputs.yaml"
+sed -i "s/sshUserName: .*/sshUserName: $username/" "$newoutput.yaml"
+
+
+sed -i "/^hosts:/,/^-/ {
+    /^- host:/ {
+        s/\(\".*\"\)/\"$host1\"/
+        n
+        s/\(\".*\"\)/\"$host2\"/
+        n
+        s/\(\".*\"\)/\"$host3\"/
+    }
+}" "$newoutput.yaml"
+
+echo "Replacement completed successfully."
